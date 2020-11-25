6a2a4a021c1f17c62ee69491ca18c97bbecf2b04
Usamaamjad56 <usamaamjad56@gmail.com>
2020-11-26 02:45:53 +0500

    use of refactored code || Lecture 57 || Part 04

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -20,10 +21,18 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                   child: RepeatContainerCode(
                 colors: Color(0xFF1D1E33),
+                cardWidget: RepeatTextAndIconWidget(
+                  iconData: FontAwesomeIcons.male,
+                  label: 'Male',
+                ),
               )),
               Expanded(
                   child: RepeatContainerCode(
                 colors: Color(0xFF1D1E33),
+                cardWidget: RepeatTextAndIconWidget(
+                  iconData: FontAwesomeIcons.female,
+                  label: 'Female',
+                ),
               ))
             ],
           )),
@@ -55,13 +64,40 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class RepeatTextAndIconWidget extends StatelessWidget {
+  RepeatTextAndIconWidget({@required this.iconData, this.label});
+  final IconData iconData;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          iconData,
+          size: 85.0,
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(fontSize: 20.0, color: Color(0xFF8D8E98)),
+        )
+      ],
+    );
+  }
+}
+
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
+  RepeatContainerCode({@required this.colors, this.cardWidget});
   final Color colors;
+  final Widget cardWidget;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardWidget,
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
